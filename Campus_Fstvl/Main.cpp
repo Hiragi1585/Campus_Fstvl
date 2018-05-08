dbcab73ab80cc7c7a83705dae1b306345b58b5ae
Shimaoka Shuya <k624204@kansai-u.ac.jp>
2018-05-08 22:10:03 +0900

    Consctruting Scene system 2 Complete BaseSceneClass

--- Campus_Fstvl/Main.cpp
+++ Campus_Fstvl/Main.cpp
@@ -17,11 +17,31 @@ enum eScene{
 	CREDIT,
 };
 
+
 class BaseScene {
-	//Base class of scene
+protected:
+	int _sceneparam;
+	bool _isChange;
+	int _next;
+
+public:
+	BaseScene() {}
+
+	virtual void init() {
+		_isChange = false;
+		_next = -1;
+	}
+	virtual void all() {
+		update();
+		draw();
+	}
+
+private:
+	virtual bool update() { return true; }
+	virtual bool draw() { return true; }
 };
 
-class Title {
+class Title : public BaseScene{
 private:
 	Array<String> _menutext = {
 		U"Game Start!",
@@ -30,31 +50,41 @@ private:
 		U"Exit"
 	};
 
-	Font font;
-
-
-	int _sceneparam;
+	Font _font;
 
 public:
-	Title() {
-		font = Font(32);
+	Title() {}
+
+	void init() override {
+		_isChange = false;
+		_next = -1;
+		_font = Font(32);
 	}
 
-	int all() {
-		update();
-		draw();
-	}
+
 private:
-	bool update() {
+	bool update() override{
 		return true;
 	}
 
-	bool draw() {
+	bool draw() override {
 		for (auto [i,text] : Indexed(_menutext)) {
-			font(text).draw(0, i * 32, ColorF(1.0, 1.0, 1.0));
+			_font(text).draw(0, i * 32, ColorF(1.0, 1.0, 1.0));
 		}
 		return true;
 	}
+};
+
+class Game : public BaseScene {
+
+};
+
+class Result : public BaseScene {
+
+};
+
+class Credit : public BaseScene {
+
 };
 
 void Main()
@@ -66,9 +96,32 @@ void Main()
 	int scene = eScene::TITLE;
 
 	Title title;
+	Game game;
+	Result result;
+	Credit credit;
+	title.init();
 
 	while (System::Update())
 	{
-		title.all();
+		switch (scene) {
+
+			case eScene::TITLE: {
+				title.all();
+				break;
+			}
+
+			case eScene::GAME: {
+				break;
+			}
+
+			case eScene::RESULT: {
+				break;
+			}
+
+			case eScene::CREDIT: {
+				break;
+			}
+
+		}
 	}
 }
