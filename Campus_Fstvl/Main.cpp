e486c422ca79501ae49f357eaf42da508f779587
Shimaoka Shuya <k624204@kansai-u.ac.jp>
2018-05-08 15:16:53 +0900

    Initial commit

--- Campus_Fstvl/Main.cpp
+++ Campus_Fstvl/Main.cpp
@@ -1,5 +1,13 @@
-﻿
-# include <Siv3D.hpp> // OpenSiv3D v0.2.5
+﻿#include <Siv3D.hpp> // OpenSiv3D v0.2.5
+
+//////////////////////////////////////////////
+//		For Campus Festival Game
+//		Programmed by Hiragi and Sekiya
+//      Created in 5/8
+//		
+//		Use OpenSiv3D Version 0.2.5
+//		CC License BY SA NC
+//////////////////////////////////////////////
 
 void Main()
 {
